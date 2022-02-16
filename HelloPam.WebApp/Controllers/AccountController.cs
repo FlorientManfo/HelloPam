043addbe42f7394e6cd2728f072a72cd06ac52a4
Florient Manfo <manfoflorient@outlook.fr>
2022-02-16 16:16:30 +0100

    Init web app

---
+++ HelloPam.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloPam.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View("");
+        }
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index");
+            //return View("Index");
+        }
+    }
+}
